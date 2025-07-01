52d23b07eef013913b00b67889c875383381623b
KD <itzkushan@gmail.com>
2025-07-01 16:26:05 +0530

    upgrades

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from my_project import logger
+from src.my_project import logger
+from src.my_project.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 
+STAGE_NAME = "Data Ingestion Stage"
 
-logger.info("Logging has been set up .")
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
